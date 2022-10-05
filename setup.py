18d54db2003481f4d4eacbd6820af3677b777365
wolfcomp <magnus1997.mho@gmail.com>
2022-10-06 00:44:30 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -12,19 +12,41 @@ docs_requirements = [
     'pdoc3',
 ]
 
-setup(
-    name='nari-act',
-    version='0.1.0',
-    author='Nonowazu',
-    author_email='oowazu.nonowazu@gmail.com',
-    description='ACT-specific additions for nari',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    python_requires='>=3.10',
-    packages=find_namespace_packages(include=['nari.ext.*']),
-    package_data={'nari.ext.act': ['py.typed']},
-    extras_require={
-        'dev': dev_requirements,
-        'docs': docs_requirements,
-    },
-)
+version = '0.1.0'
+
+try:
+    from setuptools_rust import Binding, RustExtension
+    setup(
+        name='nari-act',
+        version=version,
+        author='Nonowazu',
+        author_email='oowazu.nonowazu@gmail.com',
+        description='ACT-specific additions for nari',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        python_requires='>=3.10',
+        packages=find_namespace_packages(include=['nari.ext.*']),
+        rust_extensions=[RustExtension("nari_act_rust", debug=False, quiet=True, binding=Binding.PyO3)],
+        package_data={'nari.ext.act': ['py.typed']},
+        extras_require={
+            'dev': dev_requirements,
+            'docs': docs_requirements,
+        },
+    )
+except ImportError:
+    setup(
+        name='nari-act',
+        version=version,
+        author='Nonowazu',
+        author_email='oowazu.nonowazu@gmail.com',
+        description='ACT-specific additions for nari',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        python_requires='>=3.10',
+        packages=find_namespace_packages(include=['nari.ext.*']),
+        package_data={'nari.ext.act': ['py.typed']},
+        extras_require={
+            'dev': dev_requirements,
+            'docs': docs_requirements,
+        },
+    )
