0d49cbf0c43a1df2e5299e9a4e409723a12c2ce9
wolfcomp <magnus1997.mho@gmail.com>
2022-10-10 13:00:11 +0200

    fix function readibility

--- nari/ext/act/utils.py
+++ nari/ext/act/utils.py
@@ -4,27 +4,29 @@ from hashlib import sha256
 
 from nari.types import Timestamp
 
-from nari.ext.act.exceptions import InvalidActChecksumAlgorithm
-
 
 DEFAULT_DATE_FORMAT: str = '%Y-%m-%dT%H:%M:%S.%f%z'
 
 
 try:
-    from nari_act_rust.utils import date_from_cs_string, validate_checksum_internal
+    from nari_act_rust.utils import date_from_cs_string, validate_checksum_internal as validate_checksum_internal_rs
 except ImportError:
     def date_from_cs_string(datestr: str) -> Timestamp:
         """Look, this is dirty. This is wrong. Please someone find a better way to do this."""
         return int(datetime.strptime(f'{datestr[:26]}{datestr[-6:]}', DEFAULT_DATE_FORMAT).timestamp() * 1000)
 
-    def validate_checksum_internal(line: str, index: int) -> bool:
-        """Validates an ACT log line internal function"""
-        parts = line.split('|')
-        check_hash = parts[-1]
-        to_hash = f'{"|".join(parts[:-1])}|{index}'.encode('utf-8')
+    validate_checksum_internal_rs = None
 
-        return sha256(to_hash).hexdigest()[:16] == check_hash
 
+def validate_checksum_internal_py(line: str, index: int) -> bool:
+    """Validates an ACT log line internal function"""
+    parts = line.split('|')
+    check_hash = parts[-1]
+    to_hash = f'{"|".join(parts[:-1])}|{index}'.encode('utf-8')
+
+    return sha256(to_hash).hexdigest()[:16] == check_hash
+
+validate_checksum_internal = validate_checksum_internal_rs or validate_checksum_internal_py
 
 def date_from_act_timestamp(datestr: str) -> Timestamp:
     """Parse timestamp from ACT log into a Timestamp"""
